a063792ad44552873201f61a28df8b65f5dc4da9
Matthew Sedam <matthewsedam@google.com>
2025-04-16 14:07:39 -0700

    Fix formatting in ContextHubNanoAppRequirementsTestExecutor
    Bug: N/A
    Flag: EXEMPT bugfix
    Test: Presubmits
    Change-Id: I96d8a3c0805ab55ab968447c19bae3828b633e4b

--- java/test/chqts/src/com/google/android/chre/test/chqts/ContextHubNanoAppRequirementsTestExecutor.java
+++ java/test/chqts/src/com/google/android/chre/test/chqts/ContextHubNanoAppRequirementsTestExecutor.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dev.chre.rpc.proto.ChreApiTest;
+
 public class ContextHubNanoAppRequirementsTestExecutor extends ContextHubChreApiTestExecutor {
     private final List<Long> mPreloadedNanoappIds;
 
@@ -48,19 +49,14 @@ public class ContextHubNanoAppRequirementsTestExecutor extends ContextHubChreApi
     private static final int MAX_AUDIO_SOURCES_TO_TRY = 10;
 
     /**
-     * Formats for audio that can be provided to a nanoapp. See enum chreAudioDataFormat in the
-     * CHRE API.
+     * Formats for audio that can be provided to a nanoapp. See enum chreAudioDataFormat in the CHRE
+     * API.
      */
     public enum ChreAudioDataFormat {
-        /**
-         * Unsigned, 8-bit u-Law encoded data as specified by ITU-T G.711.
-         */
+        /** Unsigned, 8-bit u-Law encoded data as specified by ITU-T G.711. */
         CHRE_AUDIO_DATA_FORMAT_8_BIT_U_LAW(0),
 
-        /**
-         * Signed, 16-bit linear PCM data. Endianness must be native to the local
-         * processor.
-         */
+        /** Signed, 16-bit linear PCM data. Endianness must be native to the local processor. */
         CHRE_AUDIO_DATA_FORMAT_16_BIT_SIGNED_PCM(1);
 
         private final int mId;
@@ -72,7 +68,7 @@ public class ContextHubNanoAppRequirementsTestExecutor extends ContextHubChreApi
         /**
          * Returns the ID.
          *
-         * @return int      the ID
+         * @return int the ID
          */
         public int getId() {
             return mId;
@@ -82,39 +78,31 @@ public class ContextHubNanoAppRequirementsTestExecutor extends ContextHubChreApi
     public ContextHubNanoAppRequirementsTestExecutor(NanoAppBinary nanoapp) {
         super(nanoapp);
         mPreloadedNanoappIds = new ArrayList<Long>();
-        for (long nanoappId: mContextHubManager.getPreloadedNanoAppIds(mContextHub)) {
+        for (long nanoappId : mContextHubManager.getPreloadedNanoAppIds(mContextHub)) {
             mPreloadedNanoappIds.add(nanoappId);
         }
     }
 
-    /**
-     * Tests for specific sensors for activity.
-     */
+    /** Tests for specific sensors for activity. */
     public void assertActivitySensors() throws Exception {
         findDefaultSensorAndAssertItExists(CHRE_SENSOR_TYPE_INSTANT_MOTION_DETECT);
         int accelerometerHandle =
                 findDefaultSensorAndAssertItExists(CHRE_SENSOR_TYPE_ACCELEROMETER);
-        getSensorInfoAndVerifyInterval(accelerometerHandle,
-                CHRE_SENSOR_ACCELEROMETER_INTERVAL_NS);
+        getSensorInfoAndVerifyInterval(accelerometerHandle, CHRE_SENSOR_ACCELEROMETER_INTERVAL_NS);
     }
 
-    /**
-     * Tests for specific sensors for movement.
-     */
+    /** Tests for specific sensors for movement. */
     public void assertMovementSensors() throws Exception {
         findDefaultSensorAndAssertItExists(CHRE_SENSOR_TYPE_ACCELEROMETER);
-        int gyroscopeHandle =
-                findDefaultSensorAndAssertItExists(CHRE_SENSOR_TYPE_GYROSCOPE);
-        getSensorInfoAndVerifyInterval(gyroscopeHandle,
-                CHRE_SENSOR_GYROSCOPE_INTERVAL_NS);
+        int gyroscopeHandle = findDefaultSensorAndAssertItExists(CHRE_SENSOR_TYPE_GYROSCOPE);
+        getSensorInfoAndVerifyInterval(gyroscopeHandle, CHRE_SENSOR_GYROSCOPE_INTERVAL_NS);
 
-        findAudioSourceAndAssertItExists(CHRE_AUDIO_MIN_BUFFER_SIZE_NS,
+        findAudioSourceAndAssertItExists(
+                CHRE_AUDIO_MIN_BUFFER_SIZE_NS,
                 ChreAudioDataFormat.CHRE_AUDIO_DATA_FORMAT_16_BIT_SIGNED_PCM);
     }
 
-    /**
-     * Tests for specific BLE capabilities.
-     */
+    /** Tests for specific BLE capabilities. */
     public void assertBleSensors() throws Exception {
         // TODO(b/262043286): Enable this once BLE is available
         /*
@@ -124,9 +112,7 @@ public class ContextHubNanoAppRequirementsTestExecutor extends ContextHubChreApi
         */
     }
 
-    /**
-     * Returns true if the nanoappId represents a preloaded nanoapp; false otherwise.
-     */
+    /** Returns true if the nanoappId represents a preloaded nanoapp; false otherwise. */
     public boolean isNanoappPreloaded(long nanoappId) {
         return mPreloadedNanoappIds.contains(nanoappId);
     }
@@ -134,61 +120,65 @@ public class ContextHubNanoAppRequirementsTestExecutor extends ContextHubChreApi
     /**
      * Finds the default sensor for the given type and asserts that it exists.
      *
-     * @param sensorType        the type of the sensor (constant)
-     *
-     * @return                  the handle of the sensor
+     * @param sensorType the type of the sensor (constant)
+     * @return the handle of the sensor
      */
     public int findDefaultSensorAndAssertItExists(int sensorType) throws Exception {
-        ChreApiTest.ChreSensorFindDefaultInput input = ChreApiTest.ChreSensorFindDefaultInput
-                .newBuilder().setSensorType(sensorType).build();
+        ChreApiTest.ChreSensorFindDefaultInput input =
+                ChreApiTest.ChreSensorFindDefaultInput.newBuilder()
+                        .setSensorType(sensorType)
+                        .build();
         ChreApiTest.ChreSensorFindDefaultOutput response =
-                ChreApiTestUtil.callUnaryRpcMethodSync(getRpcClient(),
-                        "chre.rpc.ChreApiTestService.ChreSensorFindDefault", input);
-        Assert.assertTrue("Did not find sensor with type: " + sensorType,
-                response.getFoundSensor());
+                ChreApiTestUtil.callUnaryRpcMethodSync(
+                        getRpcClient(), "chre.rpc.ChreApiTestService.ChreSensorFindDefault", input);
+        Assert.assertTrue(
+                "Did not find sensor with type: " + sensorType, response.getFoundSensor());
         return response.getSensorHandle();
     }
 
     /**
-     * Gets the sensor samping status and verifies the minimum interval from chreGetSensorInfo
-     * is less than or equal to the expected interval -> the sensor is at least as fast at sampling
-     * as is required.
+     * Gets the sensor samping status and verifies the minimum interval from chreGetSensorInfo is
+     * less than or equal to the expected interval -> the sensor is at least as fast at sampling as
+     * is required.
      *
-     * @param sensorHandle          the handle to the sensor
-     * @param expectedInterval      the true sampling interval
+     * @param sensorHandle the handle to the sensor
+     * @param expectedInterval the true sampling interval
      */
     public void getSensorInfoAndVerifyInterval(int sensorHandle, long expectedInterval)
             throws Exception {
         ChreApiTest.ChreHandleInput input =
-                ChreApiTest.ChreHandleInput.newBuilder()
-                .setHandle(sensorHandle).build();
+                ChreApiTest.ChreHandleInput.newBuilder().setHandle(sensorHandle).build();
         ChreApiTest.ChreGetSensorInfoOutput response =
-                ChreApiTestUtil.callUnaryRpcMethodSync(getRpcClient(),
-                        "chre.rpc.ChreApiTestService.ChreGetSensorInfo", input);
-        Assert.assertTrue("Failed to get sensor info for sensor with handle: " + sensorHandle,
+                ChreApiTestUtil.callUnaryRpcMethodSync(
+                        getRpcClient(), "chre.rpc.ChreApiTestService.ChreGetSensorInfo", input);
+        Assert.assertTrue(
+                "Failed to get sensor info for sensor with handle: " + sensorHandle,
                 response.getStatus());
-        Assert.assertTrue("The sensor with handle: " + sensorHandle
-                + " does not sample at a fast enough rate.",
+        Assert.assertTrue(
+                "The sensor with handle: "
+                        + sensorHandle
+                        + " does not sample at a fast enough rate.",
                 response.getMinInterval() <= expectedInterval);
     }
 
     /**
-     * Iterates through possible audio sources to find a source that has a minimum buffer
-     * size in ns of expectedMinBufferSizeNs and a format of format.
+     * Iterates through possible audio sources to find a source that has a minimum buffer size in ns
+     * of expectedMinBufferSizeNs and a format of format.
      *
-     * @param expectedMinBufferSizeInNs         the minimum buffer size in nanoseconds (ns)
-     * @param format                            the audio format enum
+     * @param expectedMinBufferSizeInNs the minimum buffer size in nanoseconds (ns)
+     * @param format the audio format enum
      */
-    public void findAudioSourceAndAssertItExists(long expectedMinBufferSizeNs,
-            ChreAudioDataFormat format) throws Exception {
+    public void findAudioSourceAndAssertItExists(
+            long expectedMinBufferSizeNs, ChreAudioDataFormat format) throws Exception {
         boolean foundAcceptableAudioSource = false;
         for (int i = 0; i < MAX_AUDIO_SOURCES_TO_TRY; ++i) {
             ChreApiTest.ChreHandleInput input =
-                    ChreApiTest.ChreHandleInput.newBuilder()
-                    .setHandle(i).build();
+                    ChreApiTest.ChreHandleInput.newBuilder().setHandle(i).build();
             ChreApiTest.ChreAudioGetSourceOutput response =
-                    ChreApiTestUtil.callUnaryRpcMethodSync(getRpcClient(),
-                            "chre.rpc.ChreApiTestService.ChreAudioGetSource", input);
+                    ChreApiTestUtil.callUnaryRpcMethodSync(
+                            getRpcClient(),
+                            "chre.rpc.ChreApiTestService.ChreAudioGetSource",
+                            input);
             if (response.getStatus()
                     && response.getMinBufferDuration() >= expectedMinBufferSizeNs
                     && response.getFormat() == format.getId()) {
@@ -196,9 +186,12 @@ public class ContextHubNanoAppRequirementsTestExecutor extends ContextHubChreApi
                 break;
             }
         }
-        Assert.assertTrue("Did not find an acceptable audio source with a minimum buffer "
-                + "size of " + expectedMinBufferSizeNs
-                + " ns and format: " + format.name(),
+        Assert.assertTrue(
+                "Did not find an acceptable audio source with a minimum buffer "
+                        + "size of "
+                        + expectedMinBufferSizeNs
+                        + " ns and format: "
+                        + format.name(),
                 foundAcceptableAudioSource);
     }
 
