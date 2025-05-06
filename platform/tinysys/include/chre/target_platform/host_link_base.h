2daed6eb3252c1e59058aa74745bd915ccccee30
Lei Ju <leiju@google.com>
2025-05-06 14:49:58 -0700

    [Tinysys] General clean up for host link code
    Bug: 285219398
    Test: chqts on a tinysys device
    Change-Id: Ide30059b5f6f893a481d4d905219bd706619bb83

--- platform/tinysys/include/chre/target_platform/host_link_base.h
+++ platform/tinysys/include/chre/target_platform/host_link_base.h
@@ -17,13 +17,10 @@
 #ifndef CHRE_PLATFORM_TINYSYS_HOST_LINK_BASE_H_
 #define CHRE_PLATFORM_TINYSYS_HOST_LINK_BASE_H_
 
-#include <cinttypes>
 #include <cstddef>
 
 #include "chre/platform/atomic.h"
-#include "chre/platform/mutex.h"
 #include "chre/platform/shared/host_protocol_chre.h"
-#include "chre/util/lock_guard.h"
 
 namespace chre {
 
@@ -40,24 +37,21 @@ void sendDebugDumpResultToHost(uint16_t hostClientId, const char *debugStr,
 class HostLinkBase {
  public:
   HostLinkBase();
-  ~HostLinkBase();
-
   static void vChreReceiveTask(void * /*pvParameters*/);
   static void vChreSendTask(void * /*pvParameters*/);
   static void chreIpiHandler(unsigned int /*id*/, void * /*prdata*/,
                              void * /*data*/, unsigned int /*len*/);
-  void initializeIpi(void);
+  void initializeIpi();
 
   /**
    * Implements the IPC message receive handler.
    *
-   * @param cookie An opaque pointer that was provided to the IPC driver during
-   *        callback registration.
+   * @param instance An opaque pointer that was provided to the IPC driver
+   * during callback registration.
    * @param message The host message sent to CHRE.
    * @param messageLen The host message length in bytes.
    */
-  static void receive(HostLinkBase * /*instance*/, void * /*message*/,
-                      int /*messageLen*/);
+  static void receive(HostLinkBase *instance, void *message, size_t messageLen);
   /**
    * Send a message to the host.
    *
@@ -65,7 +59,7 @@ class HostLinkBase {
    * @param dataLen Size of the message payload in bytes.
    * @return true if the operation succeeds, false otherwise.
    */
-  static bool send(uint8_t * /*data*/, size_t /*dataLen*/);
+  static bool send(uint8_t *data, size_t dataLen);
 
   /**
    * Enqueues a nanoapp token database info message to be sent to the host if a
@@ -85,7 +79,7 @@ class HostLinkBase {
     mInitialized = initialized;
   }
 
-  bool isInitialized() const {
+  [[nodiscard]] bool isInitialized() const {
     return mInitialized;
   }
 
@@ -101,9 +95,8 @@ class HostLinkBase {
    * @param logMessageSize length of the log message buffer
    * @param numLogsDropped the number of logs dropped since CHRE started
    */
-  void sendLogMessageV2(const uint8_t * /*logMessage*/,
-                        size_t /*logMessageSize*/,
-                        uint32_t /*num_logs_dropped*/);
+  void sendLogMessageV2(const uint8_t *logMessage, size_t logMessageSize,
+                        uint32_t numLogsDropped) const;
 
   /**
    * Enqueues a NAN configuration request to be sent to the host.
