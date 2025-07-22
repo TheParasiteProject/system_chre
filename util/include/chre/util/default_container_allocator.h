bfb51e052fad37def81c4e4e936bd402f33f9465
Matthew Sedam <matthewsedam@google.com>
2025-07-22 14:02:50 -0700

    CHRE utils: DefaultContainerAllocator
    This class implements the pw::Allocator interface using the existing container support library. This will be used as the
    default allocator for all CHRE containers.
    
    Bug: 422577795
    Flag: EXEMPT CHRE change
    Test: Run chre_unit_tests
    Change-Id: Iab0d03d1014091864258d550d950435865f22bed

---
+++ util/include/chre/util/default_container_allocator.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CHRE_UTIL_DEFAULT_CONTAINER_ALLOCATOR_H_
+#define CHRE_UTIL_DEFAULT_CONTAINER_ALLOCATOR_H_
+
+#include "chre/util/container_support.h"
+
+#include "pw_allocator/allocator.h"
+#include "pw_allocator/capability.h"
+#include "pw_allocator/layout.h"
+
+namespace chre {
+
+//! A default implementation of the pw::Allocator interface that uses the
+//! the existing container_support.h functionality. When used with the framework
+//! or as a part of a static nanoapp, the framework's allocation mechanism
+//! will be used. When used in a dynamic nanoapp, chreHeapAlloc and chreHeapFree
+//! will be used instead.
+class DefaultContainerAllocator : public pw::Allocator {
+ public:
+  static constexpr pw::allocator::Capabilities kCapabilities = 0;
+
+  void *DoAllocate(pw::allocator::Layout layout) override {
+    return memoryAlloc(layout.size());
+  }
+
+  void DoDeallocate(void *ptr) override {
+    memoryFree(ptr);
+  }
+};
+
+}  // namespace chre
+
+#endif  // CHRE_UTIL_DEFAULT_CONTAINER_ALLOCATOR_H_
