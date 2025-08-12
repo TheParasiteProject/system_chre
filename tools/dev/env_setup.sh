f180f6f7a0d1c4e120c709849eb2398c5c031cc0
Lei Ju <leiju@google.com>
2025-08-12 14:27:51 -0700

    [Dev tools] Setup python virtual environment when possible
    Bug: 374392644
    Test: local test
    Change-Id: I1e5a5ab81a812b62b0b808d76d06e21d70028e9c

--- tools/dev/env_setup.sh
+++ tools/dev/env_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #
 # Copyright 2025, The Android Open Source Project
 #
@@ -16,12 +15,138 @@
 # limitations under the License.
 #
 
+# This script provides a set of shell functions to configure and manage the
+# CHRE (Context Hub Runtime Environment) development environment. It should be
+# sourced by the user's shell to make the functions available in their
+# terminal session.
+#
+# The main entry point is the `chre_lunch` function, which interactively sets
+# up environment variables by calling the companion `env_setup.py` script.
+# It also provides helper functions for building (`chre_make`), flashing
+# (`chre_flash`), and inspecting the environment (`chre_envs`).
+#
 export CHRE_DEV_SCRIPT_PATH=`dirname "$(realpath "${BASH_SOURCE[0]}")"`
 
 pushd $CHRE_DEV_SCRIPT_PATH > /dev/null
 source ../common.sh
 popd > /dev/null
 
+#######################################
+# Sets up pyenv to use the required Python version.
+# Globals:
+#   CHRE_PYTHON_VERSION
+# Arguments:
+#   None
+#######################################
+_setup_pyenv() {
+    # Check for pyenv
+    if ! command -v pyenv &> /dev/null; then
+        echo "pyenv is not installed. Please install pyenv and try again."
+        return 1
+    fi
+
+    # Install Python version if not already present.
+    # sudo is used because it's possible that the user doesn't have permission to modify
+    # /usr/share/pyenv/versions. Once the python version is installed sudo permission is not needed.
+    if ! sudo pyenv versions | grep -q "$CHRE_PYTHON_VERSION"; then
+        echo "Python version $CHRE_PYTHON_VERSION not found. Installing..."
+        sudo pyenv install "$CHRE_PYTHON_VERSION"
+    fi
+
+    # Make sure the correct python version is used to set up virtual environment
+    sudo pyenv local $CHRE_PYTHON_VERSION
+    export PATH=$HOME/.pyenv/shims:$PATH
+}
+
+#######################################
+# Installs required Python packages from requirements files.
+# Globals:
+#   CHRE_DEV_SCRIPT_PATH
+# Arguments:
+#   None
+#######################################
+_install_py_pkgs() {
+  local general_requirements="${CHRE_DEV_SCRIPT_PATH}/requirements.txt"
+  local protobuf_requirements="${CHRE_DEV_SCRIPT_PATH}/requirements_protobuf.txt"
+  echo -e "\nThe following python packages would be installed if they are not yet.\n"
+  cat $general_requirements
+  echo ""
+  cat $protobuf_requirements
+  echo -ne "\nInstalling python packages..."
+  pip install -r "$general_requirements" >/dev/null 2>&1 && \
+  pip install -r "$protobuf_requirements" >/dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    echo -e "\nFailed to install python packages."
+    return 1
+  else
+    echo -e "Done\n"
+    pip list
+  fi
+}
+
+#######################################
+# Sets up a Python virtual environment for CHRE development.
+#
+# This function checks if a virtual environment is active. If not, it prompts
+# the user to create one. It handles using `pyenv` to switch to the correct
+# Python version if necessary before creating the virtual environment and
+# installing packages.
+# Globals:
+#   CHRE_PYTHON_VERSION
+#   CHRE_DEV_SCRIPT_PATH
+#   VIRTUAL_ENV
+# Arguments:
+#   None
+#######################################
+_setup_python_virtual_env() {
+  local current_py_version=$(python --version | awk '{print $2}')
+  if [[ -n "$CHRE_PYTHON_VERSION" && "$current_py_version" != "$CHRE_PYTHON_VERSION" ]]; then
+    local need_a_different_py_version=true
+  fi
+
+  echo "Preparing to install python packages..."
+  if [[ -z "$VIRTUAL_ENV" ]]; then
+    echo "The installation of python packages must be done in a virtual environment."
+    echo -n "Shall we create one for you? [Y/n] "
+    read user_response
+    if [[ "$user_response" == "y" || "$user_response" == "Y" || "$user_response" == "" ]]; then
+      if [[ "$need_a_different_py_version" == "true" ]]; then
+        echo "setting up pyenv..."
+        _setup_pyenv || return 1
+      fi
+      # Create and activate the virtual environment
+      local chre_venv_path="/tmp/chre_venv"
+      if [[ -d "$chre_venv_path" ]]; then
+        echo "'${chre_venv_path}' exists. Removing it..."
+        rm -rf "$chre_venv_path"
+      fi
+      echo "Creating a new virtual environment '${chre_venv_path}' with Python $CHRE_PYTHON_VERSION..."
+      python -m venv "$chre_venv_path" || return 1
+      echo "Activating virtual environment..."
+      source ${chre_venv_path}/bin/activate
+    else
+      echo -e "\nPlease create a virtual environment and try again."
+      return 1
+    fi
+  else
+    echo "It looks like you are already in a virtual environment ${VIRTUAL_ENV}. Great!"
+    if [[ "$need_a_different_py_version" == "true" ]]; then
+      echo "The virtual environment has python version $current_py_version but $CHRE_PYTHON_VERSION is required."
+      echo "Please deactivate the current virtual environment and try again."
+      return 1
+    fi
+  fi
+
+  _install_py_pkgs || return 1
+}
+
+#######################################
+# Prints all currently configured CHRE environment variables.
+# Globals:
+#   CHRE_ENVS (array of environment variable names)
+# Arguments:
+#   None
+#######################################
 chre_envs() {
   echo ""
   for env in "${CHRE_ENVS[@]}"; do
@@ -30,6 +155,16 @@ chre_envs() {
   echo ""
 }
 
+#######################################
+# The main entry point for setting up the CHRE development environment.
+#
+# This function invokes `env_setup.py` to interactively prompt the user for
+# configuration values. It then processes the output, which consists of
+# `export` commands and `action_*` calls, to configure the shell session.
+# Finally, it sets up the Python virtual environment.
+# Arguments:
+#   A platform-target string, e.g., "qsh-nanoapp".
+#######################################
 chre_lunch() {
   echo ""
   echo "============ Setting up environment for CHRE development ============"
@@ -52,7 +187,8 @@ chre_lunch() {
     if [[ $? -ne 0 ]]; then
       onExit && return 1
     fi
-  done 3<<< "$commands"
+  done 3<<< "$commands" && \
+  _setup_python_virtual_env
 
   if [[ $? -ne 0 ]]; then
     onExit && return 1
@@ -63,5 +199,3 @@ chre_lunch() {
    onSuccess
   fi
 }
-
-
